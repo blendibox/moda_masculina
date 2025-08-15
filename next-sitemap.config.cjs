4774dc838861f5de86563443d652815f56b8bc47
blendibox <loja@blendibox.com.br>
2025-08-15 15:05:36 -0300

    removido lfs

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   siteUrl:  'https://modamasculina.blendibox.com.br',
   generateRobotsTxt: false,
   sitemapSize: 5000,
+   exclude: [
+	  "/404",
+	  "/*/__dummy__"
+	],
   outDir: './out',
   robotsTxtOptions: {
     policies: [
